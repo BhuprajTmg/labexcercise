4428fc7b09774aa077e9daaeb0010d6cfb9ab65c
hp <tsaroj896@gmail.com>
2021-02-23 20:49:38 +0545

    myname

---
+++ lab excersices/myname2.py
@@ -0,0 +1,7 @@
+#Write a program to write any ones name in form of
+#My name is.........and iam....... years old.
+
+name=str(input("enter your name:"))
+age=int(input("enter your age:"))
+
+print("My name is", +name," and I'am ",age,"years old")
