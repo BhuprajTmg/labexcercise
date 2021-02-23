3e17aeeefefed09df6803d3b3e83cc958b61b619
hp <tsaroj896@gmail.com>
2021-02-23 20:42:18 +0545

    myname

--- lab excersices/myname.py
+++ lab excersices/myname.py
@@ -1,1 +1,5 @@
-)
+name=str(input("enter your name:"))
+age=int(input("enter your age:"))
+
+
+print("My name is %s and iam %d years old" %(name, age))
--- lab excersices/readNandK.py
+++ lab excersices/readNandK.py
@@ -2,11 +2,3 @@
 #part remains in the basket. How many apple will each single student get? How many apples will be remains in the baske?
 # The program reads the number N and K. It should print the two answrer for the above question.
 
-N=int(input("enter the number of apple in the basket:"))
-K=int(input("enter the number of apple in the basket:"))
-
-D=K/N
-R=K//N
-
-print("the no of apple each student get is",D)
-print("the number of apple remains in the basket is ", R)
