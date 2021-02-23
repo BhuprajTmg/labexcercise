c4a2a49f504a8ca70ef08d088d1d83392d27747e
hp <tsaroj896@gmail.com>
2021-02-23 11:24:56 +0545

    lab1

---
+++ lab excersices/lab1.py
@@ -0,0 +1,10 @@
+#Write the progam that takes three number and print their sum.Every nunber is given on a saperate number.
+
+num1=float(input("enter the num1:"))
+num2=float(input("enter the num2:"))
+num3=float(input("enter the num3:"))
+
+sum= num1 +num2 +num3
+
+
+print(f"the sum of the given three number is : {sum}")
