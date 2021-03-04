db0a8e36ce632c7f35049eff61d6460d2aed4f38
hp <tsaroj896@gmail.com>
2021-03-04 09:55:27 +0545

    clock4

---
+++ lab excersices/clock4.py
@@ -0,0 +1,10 @@
+#Given the integer N- the number of minutes that is passed since mignight - how many hour and minute are displyed on
+#24 hour digital clock?
+#The program should print the two numbers. the number of hours(between 0- 23) and the number of minute (between 0-50)
+#For eg, if N=150 then 150 minute have passed since midnight  ie. now is 2:30 am So the program should print 2:20.
+
+N=int(input("enter the minutes passed since mid night:"))
+Hours=N//60
+Minutes=N%60
+print(f"It is{Hours}:{Minutes} A.M now")
+
